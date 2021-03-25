a0a0d1a196398abfe22927c55a34cdbacb1f3209
A-Mosher <adam.l.mosher@gmail.com>
2021-03-24 22:04:30 -0400

    Started adding pin and userbalance variables in accounts file.

---
+++ account.js
@@ -0,0 +1,17 @@
+let userPin = 1234;
+
+let userBalance = "200.00";
+
+let userInput = prompt ("Please enter your Personal Identification Number.")
+
+let pinEntered;
+if(pinEntered == userPin){
+    return true;
+}
+else(pinEnter !== userPin);{
+    alert("Incorrect PIN entered. Please try again.");
+    userInput;
+}
+
+console.log("correct PIN");
+return("correct PIN");
